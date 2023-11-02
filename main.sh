32b984264ad150e4c9a983451868141cca8ed8f6
Zernaie <evanaterx@gmail.com>
2023-11-02 17:39:10 -0500

    edit

--- main.sh
+++ main.sh
@@ -3,16 +3,16 @@ chmod 777 /etc/apt/apt.conf.d/10periodic
 cp /etc/apt/apt.conf.d/10periodic ~/Desktop/backups/
 echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Download-Upgradeable-Packages \"1\";\nAPT::Periodic::AutocleanInterval \"1\";\nAPT::Periodic::Unattended-Upgrade \"1\";" > /etc/apt/apt.conf.d/10periodic
 chmod 644 /etc/apt/apt.conf.d/10periodic
-printTime "Daily update checks, download upgradeable packages, autoclean interval, and unattended upgrade enabled."
+echo "Daily update checks, download upgradeable packages, autoclean interval, and unattended upgrade enabled."
 
 clear
 apt update -qq
 apt upgrade -qq
 apt dist-upgrade -qq
-printTime "Ubuntu OS has checked for updates and has been upgraded."
+print "Ubuntu OS has checked for updates and has been upgraded."
 
 clear
 apt autoremove -y -qq
 apt autoclean -y -qq
 apt clean -y -qq
-printTime "All unused packages have been removed."
+echo "All unused packages have been removed."
